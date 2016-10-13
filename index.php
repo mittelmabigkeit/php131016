eeef4199a2b88a51c4bd004cc6da6a2ee3bd3ff1
mittelmabigkeit <comp422012016@mail.ru>
2016-10-13 15:16:17 +0300

    sup

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: html
+ * Date: 13.10.2016
+ * Time: 15:10
+ */
+require_once 'MyClass.php';
+$obj= New MyClass (5,2);
+$obj -> umn();
+$as = $obj -> vivod();
+echo $as;
+echo "</br>";
+$sa = $obj -> izm();
+echo $sa;
+?>
